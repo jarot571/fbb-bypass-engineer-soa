8e2da7212f16ad63d0530a8de9b371798b1633f1
krair571 <kraisorn.r@epic-consulting.net>
2025-09-02 16:45:03 +0700

    Update Form1.Designer.cs

--- Window App/Window App/Form1.Designer.cs
+++ Window App/Window App/Form1.Designer.cs
@@ -31,9 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "fbb-bypass-engineer";
         }
 
         #endregion
     }
 }
+
